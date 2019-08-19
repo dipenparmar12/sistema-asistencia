6a86413f1918b5c12fc5e768ef168d1d77a82924
Dipen <dipensavji@gmail.com>
2019-08-20 01:08:20 +0530

    TypeORM get all Teachers Core Code ( Working )
    import 'reflect-metadata';
    import * as dotenv from 'dotenv';
    import { createConnection, Connection, getConnection, getRepository } from 'typeorm';
    import * as express from 'express';
    import * as bodyParser from 'body-parser';
    import * as helmet from 'helmet';
    import { TeacherRoutes } from './routes/TeacherRoutes';
    import * as path from 'path';
    import Teacher from './entity/Teacher';
    
    // initialize configuration
    dotenv.config();
    
    //Connects to the Da  tabase -> then starts the express
    createConnection()
    	.then(async connection => {
    		// Create a new express application instance
    		const app = express();
    
    		// Call midlewares
    		app.use(helmet());
    		app.use(bodyParser.json());
    
    		app.listen(process.env.PORT, () => {
    			console.log('Server started on port ' + process.env.PORT);
    		});
    
    		//Set all routes from routes folder
    		// app.use('/', routes);
    
    		app.get('', async (req, res) => {
    			const teacherRepository = getRepository(Teacher);
    			const teachers = await teacherRepository.find();
    			console.log(teachers);
    
    			//Send the teachers object
    			res.send(teachers);
    		});
    	})
    	.catch(error => console.log(error));
    
    
    
    
    {
    	"type": "mysql",
    	"database": "test",
    	"username": "root",
    	"password": "",
    	"synchronize": true,
    	"logging": false,
    	"entities": ["src/entity/**/*.ts"],
    	"migrations": ["src/migration/**/*.ts"],
    	"subscribers": ["src/subscriber/**/*.ts"],
    	"cli": {
    		"entitiesDir": "src/entity",
    		"migrationsDir": "src/migration",
    		"subscribersDir": "src/subscriber"
    	}
    }
    
    
    
    import {
    	Column,
    	Entity,
    	PrimaryGeneratedColumn,
    	CreateDateColumn,
    	Generated,
    	UpdateDateColumn,
    	VersionColumn,
    	Unique,
    } from 'typeorm';
    
    // const Teachers = attendance_db.define('teachers', {
    // 	id: { allowNull: false, primaryKey: true, type: Sequelize.UUID, defaultValue: uuidv4() }
    // 	, username: { type: DataTypes.STRING, unique: true, allowNull: false }
    // 	, password: { type: DataTypes.TEXT, allowNull: false }
    // 	, full_name: { type: DataTypes.STRING(100), allowNull: false }
    // 	, subject: { type: DataTypes.STRING(50), allowNull: false }
    // 	, email: { type: DataTypes.STRING(100), allowNull: true }
    // 	, mobile: { type: DataTypes.STRING(100), allowNull: true }
    // 	, address: { type: DataTypes.TEXT, allowNull: true }
    // 	, profile_pic: { type: DataTypes.TEXT, allowNull: true }
    // 	, user_type: { type: DataTypes.STRING(50), allowNull: true }
    // 	, dep_id: { type: DataTypes.STRING(50), allowNull: true }
    // 	, is_logged: { type: DataTypes.TEXT, allowNull: true }
    //   });
    
    @Entity()
    @Unique(['username'])
    export default class Teacher {
    	@PrimaryGeneratedColumn('uuid')
    	id: number;
    
    	@Column()
    	@Generated('uuid')
    	uuid: string;
    
    	@Column()
    	@VersionColumn()
    	version: number;
    
    	@Column({ type: 'varchar', length: 100 })
    	username: String;
    
    	@Column()
    	password: String;
    
    	@Column()
    	name: String;
    
    	@Column({ nullable: true })
    	subject: String;
    
    	@Column({ nullable: true })
    	email: String;
    
    	@Column({ nullable: true })
    	mobile: String;
    
    	@Column({ nullable: true })
    	address: String;
    
    	@Column({ default: 'default.jpg' })
    	profile_pic: String;
    
    	@Column()
    	roll: String;
    
    	@Column({ nullable: true })
    	dep_id: String;
    
    	@Column({ type: 'text', nullable: true })
    	is_logged: string;
    
    	@Column()
    	@CreateDateColumn()
    	createdAt: Date;
    
    	@Column()
    	@UpdateDateColumn()
    	updatedAt: Date;
    
    	hashPassword() {
    		// this.password = bcrypt.hashSync(this.password, 8);
    	}
    }

--- src/entity/Teacher.ts
+++ src/entity/Teacher.ts
@@ -1,15 +1,85 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+	Column,
+	Entity,
+	PrimaryGeneratedColumn,
+	CreateDateColumn,
+	Generated,
+	UpdateDateColumn,
+	VersionColumn,
+	Unique,
+} from 'typeorm';
+
+// const Teachers = attendance_db.define('teachers', {
+// 	id: { allowNull: false, primaryKey: true, type: Sequelize.UUID, defaultValue: uuidv4() }
+// 	, username: { type: DataTypes.STRING, unique: true, allowNull: false }
+// 	, password: { type: DataTypes.TEXT, allowNull: false }
+// 	, full_name: { type: DataTypes.STRING(100), allowNull: false }
+// 	, subject: { type: DataTypes.STRING(50), allowNull: false }
+// 	, email: { type: DataTypes.STRING(100), allowNull: true }
+// 	, mobile: { type: DataTypes.STRING(100), allowNull: true }
+// 	, address: { type: DataTypes.TEXT, allowNull: true }
+// 	, profile_pic: { type: DataTypes.TEXT, allowNull: true }
+// 	, user_type: { type: DataTypes.STRING(50), allowNull: true }
+// 	, dep_id: { type: DataTypes.STRING(50), allowNull: true }
+// 	, is_logged: { type: DataTypes.TEXT, allowNull: true }
+//   });
 
 @Entity()
-export class Todo {
-	@PrimaryGeneratedColumn()
-	public id: number;
+@Unique(['username'])
+export default class Teacher {
+	@PrimaryGeneratedColumn('uuid')
+	id: number;
 
 	@Column()
-	public name: string = '';
+	@Generated('uuid')
+	uuid: string;
 
 	@Column()
-	public isComplete: boolean = false;
+	@VersionColumn()
+	version: number;
+
+	@Column({ type: 'varchar', length: 100 })
+	username: String;
+
+	@Column()
+	password: String;
+
+	@Column()
+	name: String;
+
+	@Column({ nullable: true })
+	subject: String;
+
+	@Column({ nullable: true })
+	email: String;
+
+	@Column({ nullable: true })
+	mobile: String;
+
+	@Column({ nullable: true })
+	address: String;
+
+	@Column({ default: 'default.jpg' })
+	profile_pic: String;
+
+	@Column()
+	roll: String;
+
+	@Column({ nullable: true })
+	dep_id: String;
+
+	@Column({ type: 'text', nullable: true })
+	is_logged: string;
+
+	@Column()
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@Column()
+	@UpdateDateColumn()
+	updatedAt: Date;
+
+	hashPassword() {
+		// this.password = bcrypt.hashSync(this.password, 8);
+	}
 }
-
-export default Todo;
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,52 @@
 import 'reflect-metadata';
 import * as dotenv from 'dotenv';
-import { createConnection, Connection, getConnection } from 'typeorm';
+import { createConnection, Connection, getConnection, getRepository } from 'typeorm';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
 import { TeacherRoutes } from './routes/TeacherRoutes';
 import * as path from 'path';
+import Teacher from './entity/Teacher';
 
 // initialize configuration
 dotenv.config();
 
-class MyApplication {
-	public _express: express.Application = express();
-	public port: number = parseInt(process.env.PORT);
-	public teacherRoutes: TeacherRoutes = new TeacherRoutes();
+// class MyApplication {
+// 	public _express: express.Application = express();
+// 	public port: number = parseInt(process.env.PORT);
+// 	public teacherRoutes: TeacherRoutes = new TeacherRoutes();
 
-	constructor() {
-		this.appConfig();
-		this.appRoutes();
-		// const connection = getConnection();
-		this._express.listen(this.port, () => {
-			console.log(process.env.APP_NAME + ': Started:server.js At:' + this.port);
-		});
-	}
+// 	constructor() {
+// 		this.appConfig();
+// 		this.appRoutes();
+// 		// const connection = getConnection();
+// 		this._express.listen(this.port, () => {
+// 			console.log(process.env.APP_NAME + ', Started At:' + this.port);
+// 		});
+// 	}
 
-	/**
-	 * Express Midallwares & other configration ( Cookies, static path ..etc )
-	 */
-	private appConfig(): void {
-		this._express.use(bodyParser.json());
-		this._express.use(bodyParser.urlencoded({ extended: false }));
+// 	/**
+// 	 * Express Midallwares & other configration ( Cookies, static path ..etc )
+// 	 */
+// 	private appConfig(): void {
+// 		this._express.use(bodyParser.json());
+// 		this._express.use(bodyParser.urlencoded({ extended: false }));
 
-		// serving static files
-		this._express.use(express.static('public'));
+// 		// serving static files
+// 		this._express.use(express.static('public'));
 
-		//// Public Dir
-		this._express.use('/', express.static(path.join(__dirname, '../public')));
-	}
+// 		//// Public Dir
+// 		this._express.use('/', express.static(path.join(__dirname, '../public')));
+// 	}
 
-	/**
-	 * Set All Application Routes from External Class's
-	 */
-	private appRoutes(): void {
-		this.teacherRoutes.routes(this._express);
-	}
-}
-
-new MyApplication();
+// 	/**
+// 	 * Set All Application Routes from External Class's
+// 	 */
+// 	private appRoutes(): void {
+// 		this.teacherRoutes.routes(this._express);
+// 	}
+// }
+// export default new MyApplication()._express;
 
 // https.createServer(new App().app).listen(8000, () => {
 // 	console.log('APp Started');
@@ -54,32 +54,34 @@ new MyApplication();
 
 // export default new App().app;
 
-// //Connects to the Da  tabase -> then starts the express
-// createConnection({
-// 	name: 'default',
-// 	type: 'mysql',
-// 	host: 'localhost',
-// 	username: 'test',
-// 	password: '',
-// 	database: 'test',
-// })
-// 	.then(async connection => {
-// 		// Create a new express application instance
-// 		const app = express();
+//
+//
+//
 
-// 		// Call midlewares
-// 		app.use(helmet());
-// 		app.use(bodyParser.json());
+//Connects to the Da  tabase -> then starts the express
+createConnection()
+	.then(async connection => {
+		// Create a new express application instance
+		const app = express();
 
-// 		app.listen(process.env.PORT, () => {
-// 			console.log('Server started on port ' + process.env.PORT);
-// 		});
+		// Call midlewares
+		app.use(helmet());
+		app.use(bodyParser.json());
 
-// 		//Set all routes from routes folder
-// 		// app.use('/', routes);
+		app.listen(process.env.PORT, () => {
+			console.log('Server started on port ' + process.env.PORT);
+		});
 
-// 		app.get('', (req: Request, res: Response) => {
-// 			res.send('hhelo');
-// 		});
-// 	})
-// 	.catch(error => console.log(error));
+		//Set all routes from routes folder
+		// app.use('/', routes);
+
+		app.get('', async (req, res) => {
+			const teacherRepository = getRepository(Teacher);
+			const teachers = await teacherRepository.find();
+			console.log(teachers);
+
+			//Send the teachers object
+			res.send(teachers);
+		});
+	})
+	.catch(error => console.log(error));
