c5897f098918cd4121d3efa999b84b5c1069a885
Dipen <dipensavji@gmail.com>
2019-08-22 13:37:49 +0530

    Plain dashboard page Added, Active link highlighter Js function added

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -2,16 +2,20 @@ import { Router, Request, Response } from 'express'
 
 let route = Router()
 
-route.get('/index', (req: Request, res: Response) => {
-	res.render('index')
-})
-
 route.get('/registration', (req: Request, res: Response) => {
 	res.render('teacher_registration')
 })
 
+route.get('/index', (req: Request, res: Response) => {
+	res.render('index')
+})
+
 route.get('/attendance', (req: Request, res: Response) => {
 	res.render('attendance')
+})
+
+route.get('/dashboard', (req: Request, res: Response) => {
+	res.render('dashboard')
 })
 
 export default route
