980aecdb825303a25857f6c8295224aebbba4852
Dipen <dipensavji@gmail.com>
2019-09-08 10:38:36 +0530

    Backup Commit

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -1,24 +1,53 @@
-// $.getJSON('/getDashboardData', function(data, textStatus, jqXHR) {
-//   //- console.log(myApp.flattenObject(data))
-//   let students = new Map();
-//   console.log(data, students);
-//   makePivot('#attendance_dashboard', data);
-// });
-
 $.getJSON('/getDashboardData', function(data, textStatus, jqXHR) {
+  flattenObject(data);
+  // data = flattenObject(data);
   makePivot('#attendance_dashboard', data);
 });
 
 function makePivot(selector, data, uiType = 'pivot') {
   $(selector).pivotUI(data, {
-    rows: ['student_id', 'id'],
-    cols: ['date'],
-    rendererName: 'Table'
-    // filter: (record) => {return record.present == 1},
-    // derivedAttributes:{
-    //   "teacher_id": function(mp) {
-    //       return mp["present"] == "1" ? 'PRESENTS' : 'AB';
-    //   }
-    // }
+    rows: ['student_id'],
+    cols: ['date', 'present'],
+    rendererName: 'Table',
+    // filter: record => {
+    //   return record.present == 1;
+    // },
+    derivedAttributes: {
+      P: function(mp) {
+        return mp['present'] == '1' ? 'Present' : 'Absent';
+      },
+      test: function(mp) {
+        return mp['present'] == '1' ? 1 : 0;
+      }
+    }
   });
+}
+
+function flattenObject(data) {
+  var toReturn = {};
+  for (var i in data) {
+    if (!data.hasOwnProperty(i)) continue;
+    if (typeof data[i] == 'object') {
+      var flatObject = flattenObject(data[i]);
+      for (var x in flatObject) {
+        if (!flatObject.hasOwnProperty(x)) continue;
+        toReturn[i + '.' + x] = flatObject[x];
+      }
+    } else {
+      toReturn[i] = data[i];
+    }
+  }
+
+  console.log(toReturn);
+
+  return toReturn;
+
+  // let m = new Map();
+  // let arr = new Array(data);
+  // console.log(data.flat(Infinity));
+
+  // for (const row of data) {
+  //   m.set(row.id, row);
+  // }
+  // console.log(m);
 }
